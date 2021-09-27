949664baf377fe2e4fccf099f2568963730434b0
Martin Flodin <mflodin@gmail.com>
2021-09-27 12:09:33 +0200

    Continuing tutorial

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react/addons';
 import {List, Map} from 'immutable';
-import Results from '../../src/components/Results';
+import {Results} from '../../src/components/Results';
 import {expect} from 'chai';
 import {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate} from 'react-addons-test-utils';
 
